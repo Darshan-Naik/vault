f1dff2ff1c8c58e5438f41ea886a3b046b74eea1
Darshan-Naik <darshu8314@gmail.com>
2026-01-17 14:46:12 +0530

    improved pwa

--- vite.config.ts
+++ vite.config.ts
@@ -11,25 +11,39 @@ export default defineConfig({
       injectRegister: "script",
       includeAssets: ["logo.jpeg"],
       manifest: {
-        name: "Vault - Password Manager",
-        short_name: "Vault",
-        description: "Store your passwords securely",
-        theme_color: "#ffffff",
-        background_color: "#ffffff",
-        display: "standalone",
-        icons: [
+        "name": "Vault - Password Manager",
+        "short_name": "Vault",
+        "icons": [
           {
-            src: "logo.jpeg",
-            sizes: "any",
-            type: "image/jpeg",
+            "src": "/pwa-192x192.png",
+            "sizes": "192x192",
+            "type": "image/png",
+            "purpose": "any"
           },
           {
-            src: "logo.jpeg",
-            sizes: "192x192",
-            type: "image/jpeg",
-            purpose: "any maskable",
+            "src": "/pwa-512x512.png",
+            "sizes": "512x512",
+            "type": "image/png",
+            "purpose": "any"
           },
+          {
+            "src": "/pwa-maskable-192x192.png",
+            "sizes": "192x192",
+            "type": "image/png",
+            "purpose": "maskable"
+          },
+          {
+            "src": "/pwa-maskable-512x512.png",
+            "sizes": "512x512",
+            "type": "image/png",
+            "purpose": "maskable"
+          }
         ],
+        "start_url": "/",
+        "display": "standalone",
+        "background_color": "#0f0f0f",
+        "theme_color": "#0f0f0f",
+        "description": "Store your passwords securely"
       },
       workbox: {
         navigationPreload: true,
